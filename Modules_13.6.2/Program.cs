e207a0c33b76cf90cd3e73b102a4b35f59f730aa
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-04-24 22:58:49 +0300

    Добавьте файлы проекта.

---
+++ Modules_13.6.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace Modules_13._6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Fed_w\Downloads\Text1.txt";
+            LinkedList<string> list = new LinkedList<string>();
+            var stopWatch = Stopwatch.StartNew();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string str = "";
+                while ((str = sr.ReadLine()) != null)
+                    list.AddFirst(str);
+            }
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+        }
+    }
+}
---
+++ Modules_13.6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Modules_13._6_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Fed_w\Downloads\Text1.txt";
+            List<string> list = new List<string>();
+            var stopWatch = Stopwatch.StartNew();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string str = "";
+                while ((str = sr.ReadLine()) != null)
+                    list.Add(str);
+            }
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+
+        }
+    }
+}
